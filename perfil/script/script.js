69393dbdadf9f09aa41c2001e9195328c9400a93
Kauã Santos Alves <kauasantosalves@outlook.com>
2023-08-16 22:17:49 -0300

    Initial commit

---
+++ perfil/script/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    freeMode: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    });
